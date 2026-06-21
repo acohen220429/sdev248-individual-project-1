c727c7dfd6c33d2c7acc06f1f523cf81fc5c4fa4
acohen220429 <acohen22@ivytech.edu>
2026-06-21 17:33:16 -0400

    changes

--- individual-project-#1/title_screen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var start_button: Button = $VBoxContainer/StartButton
-
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
